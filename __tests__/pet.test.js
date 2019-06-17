e4b8d22614e3cf64dda55e15110f59af7a1a5d92
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2019-06-17 22:49:31 +0100

    added tests and fixed constructor

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,7 +2,7 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('Pixie').name).toBe('Pixie');
     });
   });
   
