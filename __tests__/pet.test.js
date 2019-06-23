f4a79afaee1136224a049bbe16b6abd3492b049e
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2019-06-23 20:13:49 +0100

    added walk function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,14 +4,46 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Pixie').name).toBe('Pixie');
     });
-  });
+});
 
-  describe('growUp', () => {
-    it('increments the age by 1', () => {
-      const pet = new Pet('Pixie');
+describe('growUp', () => {
+ it('increases the age by 1', () => {
+    const pet = new Pet('Pixie');
   
-      pet.growUp();
+    pet.growUp();
   
-      expect(pet.age).toEqual(1);
+    expect(pet.age).toEqual(1);
+    });
+});
+
+describe('growUp', () => {
+    it('increases the hunger by 5', () => {
+        const pet = new Pet('Pixie');
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+}); 
+
+describe('growUp', () => {
+    it('decreases the fitness by 3', () => {
+        const pet = new Pet('Pixie');
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    });
+});
+
+describe('walk', () => {
+  
+    it('increases fitness by to a maximum of 10', () => {
+      const pet = new Pet('fido');
+  
+      pet.fitness = 8;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(10);
     });
   });
